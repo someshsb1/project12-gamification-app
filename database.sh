0382e752671d29abaa670a65b69068825de091e7
Paul Maynard <Paul.Maynard001@umb.edu>
2024-02-11 14:03:10 -0500

    Add `sudo` to database commands
    Was recieving permission denied errors

--- database.sh
+++ database.sh
@@ -14,16 +14,16 @@
 
 if [[ "$1" == "setup" ]] 
 then
-    docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=Tu1ippetal!" -p 1433:1433 --name tulip_db --hostname tulip_db --platform linux/amd64 -d mcr.microsoft.com/mssql/server:2022-latest
+    sudo docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=Tu1ippetal!" -p 1433:1433 --name tulip_db --hostname tulip_db --platform linux/amd64 -d mcr.microsoft.com/mssql/server:2022-latest
 elif [[ "$1" == "start" ]] 
 then
-    docker start tulip_db
+    sudo docker start tulip_db
 elif [[ "$1" == "stop" ]] 
 then
-    docker stop tulip_db
+    sudo docker stop tulip_db
 elif [[ "$1" == "delete" ]] 
 then
-    docker stop tulip_db && docker rm tulip_db
+    sudo docker stop tulip_db && docker rm tulip_db
 else
     echo "Invalid argument"
 fi
